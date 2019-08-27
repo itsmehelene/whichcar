9b1e3f6031dace41881d1af6467e33750582aa6d
ozge <ozgezeynep08@gmail.com>
2019-08-27 18:17:49 +0200

    compare function is added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,5 @@
+//= require sweetalert2
+//= require sweet-alert2-rails
 import "bootstrap";
 import "../components/slider";
+import "../components/compare";
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cars,only: [:index,:show] do
+  resources :cars, only: [:index] do
     collection do
-    get 'compare'
+      get 'compare'
+    end
   end
+  resources :cars,only: [:index,:show] do
     resources :favourites, only: [:create]
   end
   resources :favourites,only: [:destroy,:index]
