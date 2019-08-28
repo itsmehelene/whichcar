b16d7dc5a8d5af4d4076ae64d41004ba71dfc69b
Najia <najia.elyouzghi@gmail.com>
2019-08-28 17:24:21 +0200

    modal/big_card

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,14 @@
 //= require sweetalert2
 //= require sweet-alert2-rails
 import "bootstrap";
-import "../components/slider";
-import "../components/compare";
+// import "../components/slider";
+// import "../components/compare";
+// import "../components/move_card";
+
+
+import 'animate.css/animate.min.css'
+
+$('.modal').on('hidden.bs.modal', function (e) {
+  console.log("je suis la")
+  // do something...
+})
