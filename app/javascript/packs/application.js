a2462cb2326e7e5956e48453ad8fcb193ee9a1ab
Hélène <0302@gmail.com>
2019-08-29 09:37:34 +0200

    slider debut

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,3 +6,28 @@ import "bootstrap";
 // import "../components/move_card";
 
 import 'animate.css/animate.min.css'
+
+import 'ion-rangeslider'
+
+
+
+
+    $("#demo").ionRangeSlider({
+        type: "double",
+        grid: true,
+        min: 0,
+        max: 500000,
+        from: 1000,
+        to: 5000,
+        step: 1000,
+        skin: "round",
+        onChange: function (data) {
+            // Called every time handle position is changed
+            // console.log("---")
+            // console.log(data.from);
+            // console.log(data.to)
+            document.querySelector('#min').value = data.from
+            document.querySelector('#max').value = data.to
+
+        },
+    });
