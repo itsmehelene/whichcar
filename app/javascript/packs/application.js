af985021b0db2f2d63ced794d4060539d9e0a179
ozge <ozgezeynep08@gmail.com>
2019-09-02 19:00:03 +0200

    minheight of the navbar removed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,7 +31,7 @@ const gra = function(min, max) {
 const init = function() {
     let items = document.querySelectorAll("li");
     for (let i = 0; i < items.length; i++) {
-        items[i].style.minHeight = gra(120, 205) + "vh";
+        // items[i].style.minHeight = gra(120, 205) + "vh";
         items[i].style.background = randomColor({ luminosity: "light" });
     }
 
