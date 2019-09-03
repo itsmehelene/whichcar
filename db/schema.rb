e54136d1d94bb6e957c49b04448530c1584868de
Hélène <0302@gmail.com>
2019-09-03 20:30:11 +0200

    white and grey login un navbar

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2019_09_03_144850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
