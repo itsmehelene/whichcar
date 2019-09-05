eb16ba946a253809b08f3e48a0bb27c729d23f6a
Hélène <0302@gmail.com>
2019-09-05 09:34:51 +0200

    favourite et scroll fixed

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,7 +1,8 @@
 class FavouritesController < ApplicationController
     skip_before_action :authenticate_user!, only: :show
+
   def index
-    @favourites = Favourite.all
+    @favourites = current_user.favourites
   end
 
   def show
@@ -20,20 +21,3 @@ class FavouritesController < ApplicationController
     redirect_to favourites_path if @favourite.destroy
   end
 end
-
-
-# def create
-#   @car = Car.find(params[:car_id])
-#   @favourite = Favourite.new(car: @car, user: current_user)
-#   if @favourite.save
-#     respond_to do |format|
-#       format.html { redirect_to root_path() }
-#       format.js  # <-- will render `app/views/favourites/create.js.erb`
-#     end
-#   else
-#     respond_to do |format|
-#       format.html { redirect_to root_path() }
-#       format.js  # <-- idem
-#     end
-#   end
-# end
